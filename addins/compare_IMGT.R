99a702df4ca7f3f13e2d4b73d1c843c4247077a8
Katarzyna Gruba <katarzyna.gruba@ardigen.com>
2018-08-02 17:57:16 +0200

    improve small bugs

--- addins/compare_IMGT.R
+++ addins/compare_IMGT.R
@@ -6,22 +6,22 @@ library(tigger, quietly = T, verbose = F)
 library(doParallel, quietly = T, verbose = F)
 library(msa, quietly = T, verbose = F)
 library(dplyr, quietly = T, verbose = F, warn.conflicts = F)
-source("main/download_IMGT.R")
-source("main/filter_IMGT.R")
+library(utils)
+source("../main/download_IMGT.R")
+source("../main/filter_IMGT.R")
 options(stringsAsFactors = F)
 # =================================================== args ===================================================================
 args <- c()
-IMGT_file_1 <- NA
-#IMGT_file_1 <- NA
+IMGT_file_1 <- "NO"
 Ensembl_file_1 <- "data/2018_07_31_biomart_IG_nucleotide.txt"
-gene_to_compare <- "IGHV"
+gene_to_compare <- "IGLV"
 args[1] <- IMGT_file_1
 args[2] <- Ensembl_file_1
 args[3] <- gene_to_compare
 
 if (length(args) != 3 ) {
   stop("You have to provide 3 parameters: 
-        1. path to IMGT  data or NA
+        1. path to IMGT  data or NO
         2. path to Ensembl nucleotide data (this files should be downloaded yet)
         3. name of the gene for comparing or gene type eg: IGHV", call. = FALSE)
 } 
@@ -32,20 +32,19 @@ gene_to_compare <- args[3]
 
 # ================================================= read data =================================================================
 if (!file.exists(path_to_Ensembl)) {
-  stop("File not exist")
+  stop("Ensembl file not exist")
 }
 
 Ensembl_data <- readIgFasta(path_to_Ensembl, strip_down_name = F)
 component_nb_ensembl <- Ensembl_data %>% strsplit("") %>% unlist(use.names = F) %>% table() %>% names() %>% grep("[ACTGN]",., invert = T) %>% length()
 
-if (is.na(path_to_IMGT)) {
-  if (component_nb_ensembl == 0 & is.na(path_to_IMGT)) {
+if (path_to_IMGT %in% "NO") {
+  if (component_nb_ensembl == 0) {
     type_seq <- "nucleotide" 
-  } else if (component_nb_ensembl > 0 & is.na(path_to_IMGT)) {
+  } else if (component_nb_ensembl > 0) {
     type_seq <- "protein" 
   }
-  path_to_IMGT <- download_IMGT(type_seq)
-  #IMGT_data <- filter_IMGT(input = path_to_IMGT, chain_type = substr(gene_to_compare, 3,3), region = substr(gene_to_compare, 4, 4))
+  path_to_IMGT <- download_IMGT(type_seq, out_file = "../data")
   IMGT_data <- filter_IMGT(input = path_to_IMGT, chain_type = c("heavy", "light"), region = c("V", "D", "J"))
 } else {
   if (!file.exists(path_to_IMGT)) {
@@ -113,15 +112,15 @@ out_tbl <- do.call(rbind, unlist(out_tbl, recursive = F))
 out_tbl <- out_tbl[out_tbl$first != out_tbl$second, ]
 
 out_file_path <- file.path("../results", paste("ensembl_IMGT", gene_to_compare, type_seq, Sys.Date(), ".csv", sep = "_"))
-write.csv(out_tbl, out_file_path, row.names = F, quote = F)
+write.table(out_tbl, out_file_path, row.names = F, quote = F, sep = "\t")
 # ========================================== summary ================================
-identical_seq <- out_tbl[out_tbl$result == T, c(1,2)]
-length_diff <- out_tbl[strsplit(out_tbl$diff, ",") %>% lapply(., grep, pattern = "[A-Z]") %>% lapply(., function(x) length(x) == 0 ) %>% unlist(), ]
-df <- out_tbl[(grepl("^ENSG", out_tbl$first) & grepl("^[^ENSG]", out_tbl$second)) | (grepl("^ENSG", out_tbl$second) & grepl("^[^ENSG]", out_tbl$first)), ]  
-df_ <- split.data.frame(df, df$first) %>% lapply(., function(x) x[order(x$nb_diff, decreasing = F), ] %>% head(4)) %>% unname() %>% do.call(rbind, .)
-cat("Identical sequences:") 
-identical_seq
-cat("Different in length:")
-length_diff[!(rownames(length_diff) %in% rownames(identical_seq)), ]
-cat("Enseble sequences are the most similar to:")
-df
+# identical_seq <- out_tbl[out_tbl$result == T, c(1,2)]
+# length_diff <- out_tbl[strsplit(out_tbl$diff, ",") %>% lapply(., grep, pattern = "[A-Z]") %>% lapply(., function(x) length(x) == 0 ) %>% unlist(), ]
+# df <- out_tbl[(grepl("^ENSG", out_tbl$first) & grepl("^[^ENSG]", out_tbl$second)) | (grepl("^ENSG", out_tbl$second) & grepl("^[^ENSG]", out_tbl$first)), ]  
+# df_ <- split.data.frame(df, df$first) %>% lapply(., function(x) x[order(x$nb_diff, decreasing = F), ] %>% head(4)) %>% unname() %>% do.call(rbind, .)
+# cat("Identical sequences:") 
+# identical_seq
+# cat("Different in length:")
+# length_diff[!(rownames(length_diff) %in% rownames(identical_seq)), ]
+# cat("Enseble sequences are the most similar to:")
+# df
